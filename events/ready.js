4e34e45517523cf76b156c73029d9e0e3268b4df
MrBaskan <mrbaskan33@gmail.com>
2023-05-03 18:42:18 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,5 @@
 const { ActivityType, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require("discord.js")
 require('advanced-logs')
-const { joinVoiceChannel } = require('@discordjs/voice')
 
 module.exports = {
   name: 'ready',
@@ -15,8 +14,5 @@ module.exports = {
       ]
     })
     client.user.setStatus(`idle`)
-    const Kanal = client.channels.cache.get("1091364613322461244")
-    if(!Kanal) return
-    joinVoiceChannel({channelId: Kanal.id, guildId: Kanal.guild.id, adapterCreator: Kanal.guild.voiceAdapterCreator, selfDeaf: false, selfMute:true})
   }
 }
