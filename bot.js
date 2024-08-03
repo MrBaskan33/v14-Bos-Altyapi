37c60a92a3652b0ee84ebd399e3391cf5570aea3
MrBaskan <mrbaskan33@gmail.com>
2024-08-03 14:03:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,44 @@
-const{ Client, Partials, Discord, MessageAutoDeleteTrigger } = require('discord.js')
-const client = new Client({ intents: 131071, partials: Object.values(Partials).filter(x => typeof x === "string"), shards: 'auto' })
-const { botid, token, mongo } = require("./ayarlar.json")
-require("./slash")(client)
-client.login(token)
-require("advanced-logs")
-console.setConfig({
-  background: false,
-  timestamp: false
+const Discord = require('discord.js')
+const { readdirSync } = require("fs")
+const { createCanvas, loadImage } = require('canvas')
+const { JsonDatabase } = require("wio.db")
+const db = new JsonDatabase({databasePath: `./bot/database.json`})
+const settings = require("./settings.json")
+const client = new Discord.Client({
+  intents: [98303, 
+    Discord.GatewayIntentBits.Guilds, 
+    Discord.GatewayIntentBits.GuildMessages, 
+    Discord.GatewayIntentBits.GuildPresences, 
+    Discord.GatewayIntentBits.GuildMessageReactions, 
+    Discord.GatewayIntentBits.DirectMessages,
+    Discord.GatewayIntentBits.MessageContent   
+  ]
 })
+client.login(settings.token)
+
+client.commands = new Discord.Collection()
+
+readdirSync('./events').forEach(async file => {
+  const event = await require(`./events/${file}`)
+  if(event.once) {
+    client.once(event.name, (...args) => event.execute(...args))
+  } else {
+    client.on(event.name, (...args) => event.execute(...args))
+  }
+  console.log(`[${file}] Event loaded.`)
+})
+
+const commands = []
+readdirSync('./commands').forEach(async file => {
+  if(file.endsWith('.js')) {
+    const command = await require(`./commands/${file}`)
+    commands.push(command.data.toJSON())
+    client.commands.set(command.data.name, command)
+    console.log(`[${file}] command loaded.`)
+  }
+})
+
+const rest = new Discord.REST({version: '10'}).setToken(settings.token)
+setTimeout(async () => {
+  rest.put(Discord.Routes.applicationCommands(settings.bot), {body: commands}).catch(console.error)
+}, 500)
